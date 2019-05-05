47e039a8ca4ec6e0c65e1914c5e40fac0c02b910
Emanuel Azage <eazage@usc.edu>
2019-05-04 20:39:42 -0700

    updates access token and test user for tests

--- __tests__/api/token-test.js
+++ __tests__/api/token-test.js
@@ -5,9 +5,9 @@ const { Joi } = frisby;
 it('should return status of 200 and an access token', () => {
   return frisby
           .post('http://localhost:8080/auth/token', {
-            id: 11,
+            id: 16,
 	          email: "test@usc.edu",
-	          password: "$2y$10$n/BHaNRFmTJwOvRMU0Ug2eAHpegZcL.bAc1n9tgoSRgTejZ6Yse1G"
+	          password: "$2y$10$wno8qKbpTAmuUw8Wb1RVF.kcTvwmqMTlmjzzZGRIIYtOnh3Wy1AqC"
           },)
           .expect('status', 200)
           .expect('jsonTypes', 'token', Joi.string());
--- __tests__/api/user-test.js
+++ __tests__/api/user-test.js
@@ -2,7 +2,7 @@ const frisby = require('frisby');
 
 const { Joi } = frisby;
 
-const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXNzd29yZCI6IiQyeSQxMCRuL0JIYU5SRm1USndPdlJNVTBVZzJlQUhwZWdaY0wuYkFjMW45dGdvU1JnVGVqWjZZc2UxRyIsImlhdCI6MTU1NzAwODc4M30.j484orWb2KvN3q24K-WkQyxQ2RPZl_7OabZMabFJZrs';
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXNzd29yZCI6IiQyeSQxMCR3bm84cUticFRBbXVVdzhXYjFSVkYua2NUdndtcU1UbG1qenpaR1JJSVl0T25oM1d5MUFxQyIsImlhdCI6MTU1NzAyNzA4Nn0.OdN9myU0tWxK4i9OMLahbHfelOJpyCtNq1u_hRJma3k';
 
 frisby.globalSetup({
   request: {
@@ -22,7 +22,7 @@ it('should return status of 404 when the user is not found', (done) => {
 
 it('should return status of 200 and the user when it is found', (done) => {
     frisby
-      .get('http://localhost:8080/api/user/11')
+      .get('http://localhost:8080/api/user/16')
       .expect('status', 200)
       .expect('json', 'email', 'test@usc.edu')
       .done(done);
